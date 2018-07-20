238fd4d44a9749fd4ec0eb518d137ba90d1354c5
takhmina0907 <lollis0907@gmail.com>
2018-07-20 20:51:18 +0600

    first commit

---
+++ window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Takhmina Talipova on 7/20/18.
+//  Copyright © 2018 Takhmina Talipova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
